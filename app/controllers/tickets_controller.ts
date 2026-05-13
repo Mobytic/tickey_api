d8fa7b1bf275d836138ab6ef9356c66ab8d22b89
laura.chevillet <chevillet.laura@hotmail.fr>
2026-05-13 17:21:28 +0200

    WIP : Ticket's crud

--- app/controllers/tickets_controller.ts
+++ app/controllers/tickets_controller.ts
@@ -1,4 +1,36 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Ticket from '#models/ticket'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TicketsController {
+
+    /**
+    * Ticket index
+    */
+    public async index({ response }: HttpContext) {
+    
+        const tickets = await Ticket.all()
+
+        return response.ok(tickets)
+    }
+
+    /**
+    * Ticket's creation
+    */
+    public async create({ request, response }: HttpContext) {
+
+        const payload = request.only(['title', 'description', 'status', 'priority', 'client_id'])
+        const ticket = await Ticket.create(payload)
+        
+        return response.created(ticket)
+    }
+
+    /**
+    * Get one ticket
+    */
+    public async show({ params, response }: HttpContext) {
+
+        const ticket = await Ticket.findOrFail(params.id)
+
+        return response.ok(ticket)
+    }
 }
