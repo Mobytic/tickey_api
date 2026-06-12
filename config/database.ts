8dfc5ce83fcd8f7e57cb48ddb30670ffdb1afd61
LuckyMarty <luckymarty68@gmail.com>
2026-06-12 15:30:41 +0200

    fix: set default values for database connection parameters

--- config/database.ts
+++ config/database.ts
@@ -7,9 +7,9 @@ const dbConfig = defineConfig({
     mysql: {
       client: 'mysql2',
       connection: {
-        host: env.get('DB_HOST') || 'localhost',
-        port: env.get('DB_PORT') || 3306,
-        user: env.get('DB_USER') || 'lk7bf_tickey',
+        host: '0.0.0.0',
+        port: 3306,
+        user: 'lk7bf_tickey',
         password: env.get('DB_PASSWORD') || 'ha359??eHWS$',
         database: env.get('DB_DATABASE') || 'lk7bf_tickey',
       },
